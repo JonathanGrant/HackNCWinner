534d3252b7bb73f021101da31aecfac160a082ba
JonathanGrant <jonathanallengrant@gmail.com>
2016-10-29 14:50:55 -0700

    Just cali gigs

--- craigs.py
+++ craigs.py
@@ -14,14 +14,23 @@ class Gig:
 		rsp = requests.get(self.url, params=params)
 		# BS4 can quickly parse our text, make sure to tell it that you're giving html
 		html = bs4(rsp.text, 'html.parser')
-		self.datetime = html.find_all(datetime=True)[0]['datetime']
+		self.datetime = 'unknown'
+		if len(html.find_all(datetime=True)) > 0:
+			self.datetime = html.find_all(datetime=True)[0]['datetime']
 		links = html.find_all(href=True)
 		for link in links:
-			if 'maps.google' in link['href']:
-				longlat = link['href'].split('@')[1].split(',')
-				self.lon = longlat[0]
-				self.lat = longlat[1]
-		self.about = html.findAll(attrs={'id': 'postingbody'})[0].text.split('QR Code Link to This Post')[1].lstrip()
+			if 'maps.google.com/maps/preview' in link['href']:
+				if len(link['href'].split('@')) > 0:
+					longlat = link['href'].split('@')[1].split(',')
+					self.lon = longlat[0]
+					self.lat = longlat[1]
+				break
+		self.about = 'unknown'
+		if len(html.findAll(attrs={'id': 'postingbody'})) > 0:
+			if len(html.findAll(attrs={'id': 'postingbody'})[0].text.split('QR Code Link to This Post')) > 0:
+				self.about = html.findAll(attrs={'id': 'postingbody'})[0].text.split('QR Code Link to This Post')[1].lstrip()
+			else:
+				self.about = html.findAll(attrs={'id': 'postingbody'})[0].text.lstrip()
 		if 'lbg' in self.url:
 			self.genre = 'labor'
 		elif 'cpg' in self.url:
@@ -40,20 +49,23 @@ class Gig:
 			self.genre = 'writing'
 		else:
 			self.genre = 'other'
-		compsection = html.findAll(attrs={'class': 'attrgroup'})[0]
-		self.pay = compsection.find('b').text
+		self.pay = 'Unknown'
+		if len(html.findAll(attrs={'class': 'attrgroup'})) > 0:
+			compsection = html.findAll(attrs={'class': 'attrgroup'})[0]
+			self.pay = compsection
+			if compsection.find('b'):
+				self.pay = compsection.find('b').text
 
 
 def getAllGigsEverywhere():
 	allGigs = []
 	locations = []
-	with open('craigsLocations.txt') as f:
+	with open('locs.txt') as f:
 		locations = f.read().splitlines()
 	count = 0
 	for loc in locations:
+		print(count)
 		count += 1
-		if count > 2:
-			break
 		loc_url = 'http://' + loc + '.craigslist.org'
 		url_base = loc_url + '/search/ggg'
 		params = dict()
@@ -68,7 +80,17 @@ def getAllGigsEverywhere():
 		gigs = html.find_all('p', attrs={'class': 'row'})
 		for gig in gigs:
 			gigUrl = gig.find_all(href=True)[0]['href']
-			allGigs.append(Gig(gig.findAll(attrs={'class': 'hdrlnk'})[0].text, loc, loc_url + gigUrl))
+			myGig = Gig(gig.findAll(attrs={'class': 'hdrlnk'})[0].text, loc, loc_url + gigUrl)
+			myGig.getInfoFromCraig()
+			allGigs.append(myGig)
 	return allGigs
 
-print(getAllGigsEverywhere()[0].getInfoFromCraig())
+f = open('caligigs.csv','w')
+count = 0
+f.write(('Name,Location,URL,datetime,lon,lat,genre,pay,about' + '\n').encode('utf-8'))
+for gig in getAllGigsEverywhere():
+	print(count)
+	count += 1
+	line = gig.name + ',' + gig.location + ',' + gig.url + ',' + gig.datetime + ',' + gig.lon + ',' + gig.lat + ',' + gig.genre + ',' + gig.pay + ',' + gig.about
+	f.write((line + '\n').encode('utf-8'))
+f.close()
--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 
 app.use("/", express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 });
+
+app.get('/alllocations', function(req, res) {
+	fs.readFile('craigsLocations.txt', 'utf8', function (err,data) {
+		if (err) {
+			console.log(err);
+			return err
+		}
+		res.send(data)
+	})
+})
+
+app.get('/allgigs', function(req, res) {
+	fs.readFile('gigs.txt', 'utf8', function (err,data) {
+		if (err) {
+			console.log(err);
+			return err
+		}
+		console.log(typeof data)
+		res.send(data)
+	})
+})
 
 app.listen(process.env.PORT || 3000);
